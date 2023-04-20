82fb09126b92408b01977bbb55d6da7801ec0abe
mariecp27 <mariaher.cp@gmail.com>
2023-04-20 12:28:16 -0500

    All styles added for FAQ component

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -14,54 +14,66 @@ import {
 const FAQ = () => {
   return (
     <div className='faq'>
-      <h3>Preguntas frecuentes</h3>
-      <section className='faq__location'>
-        <h4>¿Dónde estámos?</h4>
-        <div>
-          <div className='faq_icon'>
-            <FontAwesomeIcon icon={faLocationDot} />
+      <div className='faq__body'>
+        <h3>Preguntas frecuentes</h3>
+        <section className='faq__location'>
+          <h4>¿Dónde estámos?</h4>
+          <div>
+            <div className='faq_icon'>
+              <FontAwesomeIcon icon={faLocationDot} />
+            </div>
+            <p className='m-0'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
+              augue ut mi tempus porttitor commodo vel sapien. Quisque at
+              pharetra nunc. In fermentum convallis malesuada. Nulla massa
+              neque, egestas sed velit in, hendrerit consectetur odio.
+            </p>
           </div>
-          <p className='m-0'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
-            augue ut mi tempus porttitor commodo vel sapien. Quisque at pharetra
-            nunc. In fermentum convallis malesuada. Nulla massa neque, egestas
-            sed velit in, hendrerit consectetur odio.
-          </p>
-        </div>
-      </section>
-      <section className='faq__how-to-purchase'>
-        <h4>¿Cómo comprar?</h4>
-        <div>
-          <div className='faq_icon'>
-            <FontAwesomeIcon icon={faCartShopping} />
+        </section>
+        <section className='faq__how-to-purchase'>
+          <h4>¿Cómo comprar?</h4>
+          <div>
+            <div className='faq_icon'>
+              <FontAwesomeIcon icon={faCartShopping} />
+            </div>
+            <p className='m-0'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
+              augue ut mi tempus porttitor commodo vel sapien. Quisque at
+              pharetra nunc. In fermentum convallis malesuada. Nulla massa
+              neque, egestas sed velit in, hendrerit consectetur odio.
+            </p>
           </div>
-          <p className='m-0'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
-            augue ut mi tempus porttitor commodo vel sapien. Quisque at pharetra
-            nunc. In fermentum convallis malesuada. Nulla massa neque, egestas
-            sed velit in, hendrerit consectetur odio.
-          </p>
-        </div>
-      </section>
-      <section className='faq__purchase-methods'>
-        <h4>Medios de pago</h4>
-        <div>
-          <div className='faq_icon'>
-            <FontAwesomeIcon icon={faDollar} />
+        </section>
+        <section className='faq__purchase-methods'>
+          <h4>Medios de pago</h4>
+          <div>
+            <div className='faq_icon'>
+              <FontAwesomeIcon icon={faDollar} />
+            </div>
+            <p className='m-0'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
+              augue ut mi tempus porttitor commodo vel sapien. Quisque at
+              pharetra nunc. In fermentum convallis malesuada. Nulla massa
+              neque, egestas sed velit in, hendrerit consectetur odio.
+            </p>
+            <div className='faq_purchase-methods-icons'>
+              <FontAwesomeIcon icon={faCcMastercard} />
+              <FontAwesomeIcon icon={faCcVisa} />
+              <FontAwesomeIcon icon={faCcAmex} />
+            </div>
           </div>
-          <p className='m-0'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel
-            augue ut mi tempus porttitor commodo vel sapien. Quisque at pharetra
-            nunc. In fermentum convallis malesuada. Nulla massa neque, egestas
-            sed velit in, hendrerit consectetur odio.
-          </p>
-          <div className='faq_purchase-methods-icons'>
-            <FontAwesomeIcon icon={faCcMastercard} />
-            <FontAwesomeIcon icon={faCcVisa} />
-            <FontAwesomeIcon icon={faCcAmex} />
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
+      <img
+        src={process.env.PUBLIC_URL + '/assets/images/footer_1.svg'}
+        alt='Waves'
+        className='faq__waves faq__waves-1'
+      />
+      <img
+        src={process.env.PUBLIC_URL + '/assets/images/footer_2.svg'}
+        alt='Waves'
+        className='faq__waves faq__waves-2'
+      />
     </div>
   )
 }
