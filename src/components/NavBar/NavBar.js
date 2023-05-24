6704c1cf4e615f361b82cf872cd4be95bf8b26db
mariecp27 <mariaher.cp@gmail.com>
2023-05-23 19:42:44 -0500

    Navbar bug fixed

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
@@ -9,12 +9,24 @@ import Offcanvas from 'react-bootstrap/Offcanvas'
 
 const ANavBar = ({ handleSectionChange, section }) => {
   const [color, setColor] = useState(false)
+  const [navbarExpanded, setNavbarExpanded] = useState(false)
 
   const changeColor = () => {
     if (window.scrollY >= 96) {
       setColor(true)
     } else {
       setColor(false)
+    }
+  }
+
+  const toggleNavbar = () => {
+    setNavbarExpanded(!navbarExpanded)
+  }
+
+  const handleClick = (section) => {
+    handleSectionChange(section)
+    if (window.innerWidth < 992 ) {
+      toggleNavbar()
     }
   }
 
@@ -31,7 +43,8 @@ const ANavBar = ({ handleSectionChange, section }) => {
         <Navbar
           key={expand}
           expand={expand}
-          className={`mb-3 navbar ${color ? 'navbar-bg' : ''}`}>
+          className={`mb-3 navbar ${color ? 'navbar-bg' : ''}`}
+          expanded={navbarExpanded}>
           <Container fluid>
             <Link to='/'>
               <img
@@ -40,42 +53,45 @@ const ANavBar = ({ handleSectionChange, section }) => {
                 className='navbar__logo'
               />
             </Link>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`}>
+            <div
+              className='navbar-toggler'
+              onClick={toggleNavbar}>
               <FontAwesomeIcon icon={faBars} />
-            </Navbar.Toggle>
+            </div>
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement='start'>
-              <Offcanvas.Header
-                closeButton
-                closeVariant='white'>
+              <Offcanvas.Header closeVariant='white'>
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
                   ¡Bienvenido!
+                  <button onClick={toggleNavbar}>
+                    <FontAwesomeIcon icon={faXmark} />
+                  </button>
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className='justify-content-end flex-grow-1 pe-3'>
                   <Nav.Link
-                    onClick={() => handleSectionChange(section)}
+                    onClick={() => handleClick(section)}
                     as={Link}
                     to='/home'>
                     Inicio
                   </Nav.Link>
                   <Nav.Link
-                    onClick={() => handleSectionChange(section)}
+                    onClick={() => handleClick(section)}
                     as={Link}
                     to='/products'>
                     Productos
                   </Nav.Link>
                   <Nav.Link
-                    onClick={() => handleSectionChange(section)}
+                    onClick={() => handleClick(section)}
                     as={Link}
                     to='/home/who-are-we'>
                     ¿Quiénes somos?
                   </Nav.Link>
                   <Nav.Link
-                    onClick={() => handleSectionChange(section)}
+                    onClick={() => handleClick(section)}
                     as={Link}
                     to='/home/faq'>
                     Preguntas frecuentes
